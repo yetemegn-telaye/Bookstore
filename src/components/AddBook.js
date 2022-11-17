8947831efdbe7081dec1c737f8147b626b3fc080
yetemegn-telaye <lemma19yet@gmail.com>
2022-11-18 01:19:29 +0300

    Use react-redux to dispatch the actions

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-class AddBook extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [book, setBook] = useState({});
+  const handleAddClick = () => {
+    const newBook = {
+      id: uuidv4(),
+      title: document.querySelector('#title').value,
+      author: document.querySelector('#author').value,
+    };
+    setBook(newBook);
+    dispatch(addBook(newBook));
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  };
+  console.log(book);
+  return (
+    <form>
+      <h3>Add new Book</h3>
+      <input type="text" placeholder="Book Title" name="title" id="title" />
+      <input type="author" placeholder="Author" name="author" id="author" />
+      <button
+        type="button"
+        onClick={handleAddClick}
+      >
+        Add Book
+      </button>
 
-  render() {
-    return (
-      <form>
-        <h3>Add new Book</h3>
-        <input type="text" placeholder="Book Title" />
-        <input type="author" placeholder="Author" />
-        <button type="button">Add Book</button>
-      </form>
-    );
-  }
-}
+    </form>
+  );
+};
 export default AddBook;
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 const Book = (props) => {
-  const { title, author } = props;
+  const { title, author, bookId } = props;
+  const dispatch = useDispatch();
   return (
     <div>
       <p>
@@ -10,7 +13,12 @@ const Book = (props) => {
         by
         {author}
       </p>
-      <button type="button">Remove</button>
+      <button
+        type="button"
+        onClick={() => dispatch(removeBook(bookId))}
+      >
+        Remove
+      </button>
     </div>
   );
 };
@@ -18,6 +26,7 @@ const Book = (props) => {
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  bookId: PropTypes.string.isRequired,
 };
 
 export default Book;
