f3677ff7d158e0c479cfec4078966f8dd3c17142
yetemegn-telaye <lemma19yet@gmail.com>
2023-11-24 17:00:19 +0300

    Remove alert to fix deploy error

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,24 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const AddComment = () => {
-  const [name, setName] = useState('');
-  const [comment, setComment] = useState([]);
   const bookId = useParams();
-
-  const handleAddComment = () => {
-    alert(name, comment, bookId);
-  };
 
   return (
     <>
       <h1>Add Comment</h1>
       <form>
         <div className="form-inputs">
-          <input type="text" placeholder="Name" name="name" id="name" onChange={(e) => setName(e.target.value)} />
-          <input type="text" placeholder="Comment" name="comment" id="comment" onChange={(e) => setComment(e.target.value)} />
+          {bookId}
+          <input type="text" placeholder="Name" name="name" id="name" />
+          <input type="text" placeholder="Comment" name="comment" id="comment" />
         </div>
-        <button type="button" className="add-comment-btn" onClick={handleAddComment}>Add Comment</button>
+        <button type="button" className="add-comment-btn">Add Comment</button>
       </form>
     </>
   );
