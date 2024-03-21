d65f993579d921f7cb44274ab9c570e1c1cef881
yetemegn-telaye <lemma19yet@gmail.com>
2024-03-21 15:51:32 +0300

    Install react bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
